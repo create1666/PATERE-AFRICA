54326357a4fdcb51b45fae4e1f9e044e2249db90
personal <create1666@gmail.com>
2023-04-13 22:22:38 -0400

    feat(subheadroute): refactoring

--- src/components/Reusables/SVGs/Svg.tsx
+++ src/components/Reusables/SVGs/Svg.tsx
@@ -1597,5 +1597,13 @@ export const EasyToUnderstand = (
     </defs>
   </svg>
 );
+export const MarketInsightImage = (
+  props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>
+) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={48} height={1} {...props}>
+    <title>{'Rectangle 5'}</title>
+    <path fill="#063855" fillRule="evenodd" d="M0 0h48v1H0z" />
+  </svg>
+);
 
 export default VisualRpt;
--- src/pages/marketInsights/index.tsx
+++ src/pages/marketInsights/index.tsx
@@ -1,49 +1,28 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { Box } from '@mantine/core';
 
 import { HeaderNav } from '@/components/Header/Header';
+import { MarketInsightImage } from '@/components/Reusables/SVGs/Svg';
 import SubHeader from '@/components/SubHeader/SubHeader';
 
-const NavBarLinks = [
-  { label: ' Home', href: '/home' },
-  { label: ' About Us', href: '/about' },
-  {
-    label: ' Shrefck Screener',
-    href: '/Shrefckscreener',
-  },
-  {
-    label: ' Pricing ',
-    href: '/Pricing',
-  },
-  {
-    label: 'Market Insights',
-    href: '/marketInsights',
-  },
-];
 const MarketInsightsLayout = () => {
-  const [matchedPath, setMatchRoute] = useState<string | undefined>(
-    'marketInsights'
-  );
-  const router = useRouter();
-
-  useEffect(() => {
-    const pathmatch = NavBarLinks.map((route) => route.href.split('/')[1]).find(
-      (path) => path === router.pathname.split('/')[1]
-    );
-    setMatchRoute(pathmatch);
-  }, [router.pathname]);
-
-  console.log('MTCHROUT,', matchedPath);
   return (
     <div className="">
       <HeaderNav onClick={() => ''} />
       <SubHeader />
 
-      <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, ipsa,
-        officiis tempora quod, cupiditate iste modi et id quaerat atque ipsum
-        fuga maxime saepe asperiores dolore esse? Maxime, voluptatem at?
-      </div>
+      <Box className="flex">
+        <Box>
+          <MarketInsightImage />
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, ipsa,
+          officiis tempora quod, cupiditate iste modi et id quaerat atque ipsum
+          fuga maxime saepe asperiores dolore esse? Maxime, voluptatem at?
+        </Box>
+        <Box>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, ipsa,
+          officiis tempora quod, cupiditate iste modi et id quaerat atque ipsum
+          fuga maxime saepe asperiores dolore esse? Maxime, voluptatem at?
+        </Box>
+      </Box>
     </div>
   );
 };
